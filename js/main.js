36e99653f9c04407918ed306b803d254260ccc04
Julieta <arq.julietacastillo@gmail.com>
2022-11-14 01:54:39 -0300

    Cargo 2da Pre-entrega

--- js/main.js
+++ js/main.js
@@ -128,3 +128,102 @@ function cotizar () {
 }
 
 
+
+//OBJETOS Constructores
+
+const IVA = 1.21
+
+class Productox {
+    constructor(nombre, precio, stock) {
+        this.nombre = nombre.toUpperCase()
+        this.precio = precio
+        this.stock = parseInt(stock)
+    }
+        precioFinal() {
+            return (this.precio * IVA).toLocaleString()
+        }
+
+        descontarStock(unidades) {
+            this.stock = this.stock - unidades
+        }  
+}
+
+const producto1 = new Productox("la comunidad del anillo", 2350, 30)
+const producto2 = new Productox("las dos torres", 3150, 50)
+const producto3 = new Productox("las dos torres", 4780, 25)
+
+// ARRAY
+const libros = ["La comunidad del anillo", "Las 2 Torres", "Regreso del Rey", "Los Padecientes" ]
+
+//Accede al DOM 
+function agregarLibro() {
+    let nuevoLibro = prompt("Ingresa un nuevo Título:")
+
+        if (libros.includes(nuevoLibro)) {
+            console.warn(nuevoLibro, "ya está en el listado.")            
+        } else {
+            libros.push(nuevoLibro)
+            listarLibrosHTML()
+        }
+}
+
+function buscarLibro() {
+    let libro = prompt("Ingresa el Libro a buscar:")
+        let posicion = libros.indexOf(libro)
+            if (posicion === -1) {
+                console.warn("No se encontró el libro", libro)
+            } else {
+                alert(libro + " se encuentra en la posición " + posicion)
+            }
+}
+
+function quitarLibro() {
+    let libro = prompt("Ingresa el libro a eliminar:")
+    let posicion =  libros.indexOf(libro)
+        if (posicion > -1) {
+            let eliminado = libros.splice(posicion, 1)
+            console.table(eliminado)
+            listarLibrosHTML()
+        }
+}
+
+function ordenarLibros() {
+    let sort = prompt("Ingrese la forma de ordenamiento. (A ó Z)")
+    if (sort.toUpperCase() === "A") {
+        libros.sort()
+    } else if (sort.toUpperCase() === "Z") {
+        libros.sort().reverse()
+    } else {
+        console.warn("Ingresa un tipo de ordenamiento válido. (A ó Z")
+        return 
+    }
+    listarLibrosHTML()
+}
+
+function listarLibrosHTML() {
+    const listado = document.querySelector("Ol")
+        listado.innerHTML = ""
+        for (libro of libros)
+            listado.innerHTML += `<li>${libro}</li>`
+}
+listarLibrosHTML()
+
+function retornoCard(producto) {
+    return `<div class="card" id="card${producto.id}">
+                <div class="card-image">${producto.imagen}</div>
+                <div class="card-name">${producto.nombre}</div>
+                <div class="card-price">${producto.precio}</div>
+                <div class="card-button">
+                    <button class="button button-outline button-add" id="${producto.id}"" title="Clic para agregar '${producto.nombre}' al carrito">+</button>
+                </div>
+            </div>`
+    }
+    
+function retornoError() {
+return `<div class="card-error">
+        <h2>Houston, tenemos un problema 🔌</h2>
+        <h3>No pudimos cargar los productos. 🤦🏻‍♂️</h3>
+        <h3>Intenta nuevamente en unos instantes...</h3>
+    </div>`
+}
+
---
+++ js/productos.js
@@ -0,0 +1,6 @@
+//Libros
+
+const productos = [ { id: 1, imagen: "", nombre: "La Comunidad del Anillo", precio: 3220 },
+                    { id: 2, imagen: "", nombre: "Las dos Torres", precio: 4500 },
+                    { id: 3, imagen: "", nombre: "El Retorno del Rey", precio: 5800 },
+                    { id: 4, imagen: "", nombre: "Los Padecientes", precio: 3350 } ]
