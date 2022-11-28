ffd5c21110137f1362e8e465387c7b14e33a0080
Julieta <arq.julietacastillo@gmail.com>
2022-11-28 00:33:53 -0300

    subo pre-entrega 3

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 let nombre = "Julieta"
 let apellido = "Castillo"
 let anio = 2022
-let mes = 10
+let mes = 11
 let eleccion = confirm("Este sitio web utiliza cookies para mejorar su experiencia. Al aceptar estás consintiendo nuestro uso de cookies.")
 
 const CURSO = "JavaScript"
@@ -15,215 +15,80 @@ if (respuesta == "si") {
     console.log("acceder");
 }
 
-//CICLO WHILE
-//let login = confirm("¿Desea logearse?")
-/*while(login) {
-    let usuario = prompt("Ingresa tu nombre:")
-    let passwd = prompt("Ingresa tu contraseña:")
-        if (usuario == "Julieta" && passwd == "Castillo") {
-            console.log("Bienvenid@. " + usuario)
-            login = false
-        }
-        else { 
-            console.warn("No se reconoce usuario y/o contraseña")
-        }
-}*/
+const tbody = document.querySelector("tbody")
 
-//FUNCIONES
-//debugger
-const valorFijo = 20 
-let importe = 0
-let tituloLibro = 0
-let tipoLibro = ""
-let continuar = true 
+//Guardo y Recupero el Carrito con LocalStorage + JSON
+const carrito = []
+const guardarCarrito = ()=> (carrito.length > 0) && localStorage.setItem("CarritoLibos", JSON.stringify(carrito))
+const recuperarCarrito = ()=> JSON.parse(localStorage.getItem("CarritoLibros")) || []
+carrito.push(...recuperarCarrito())
 
-const mensajeBienvenida = "Bienvenid@ a Te Leo Luego. Que libro estabas buscando? \n" +
-                          "1) El Señor de los anillos, La comunidad del anillo \n" +
-                          "2) El Señor de los anillos, Las dos Torres \n" +
-                          "3) El Señor de los anillos, El Retorno del Rey\n" +
-                          "4) Los Padecientes\n"
+//Armo tabla HTML dinámica
 
-const mensajeLibro = "¿Que formato te interesa? \n" +
-                          "A) Digital \n" + 
-                          "B) Físico \n" +
-                          "C) Con Ilustraciones \n"
-
-function cotizar () {
-    const respuesta = parseInt(prompt(mensajeBienvenida))
-        if (respuesta === NaN) {
-            console.error("Debes de ingresar un valor de acuerdo a lo sugerido")
-            return
-        }
-
-        switch(respuesta) {
-            case 1: //comunidad
-                tipoLibro = prompt(mensajeLibro).toUpperCase().trim()
-                if (tipoLibro !== "A" && tipoLibro !== "B" && tipoLibro !== "C") {
-                    console.error("no seleccionaste una opcion válida")
-                    return
-                }
-
-                if (tipoLibro === "A") {
-                    importe = valorFijo *100
-                } else if (tipoLibro === "B") {
-                    importe = valorFijo *200
-                } else {
-                    importe = valorFijo *300  
-                }                   
-                break
-
-            case 2: //torres
-                tipoLibro = prompt(mensajeLibro).toUpperCase().trim()
-                if (tipoLibro !== "A" && tipoLibro !== "B" && tipoLibro !== "C") {
-                    console.error("no seleccionaste una opcion válida")
-                    return
-                }
-
-                if (tipoLibro === "A") {
-                    importe = valorFijo *150
-                } else if (tipoLibro === "B") {
-                    importe = valorFijo *250
-                } else {
-                    importe = valorFijo *350  
-                }                   
-                break
-
-            case 3: //rey
-                tipoLibro = prompt(mensajeLibro).toUpperCase().trim()
-                if (tipoLibro !== "A" && tipoLibro !== "B" && tipoLibro !== "C") {
-                    console.error("no seleccionaste una opcion válida")
-                    return
-                }
-
-                if (tipoLibro === "A") {
-                    importe = valorFijo *200
-                } else if (tipoLibro === "B") {
-                    importe = valorFijo *300
-                } else {
-                    importe = valorFijo *400  
-                }                   
-                break
-
-            case 4: //padecientes
-                tipoLibro = prompt(mensajeLibro).toUpperCase().trim()
-                if (tipoLibro !== "A" && tipoLibro !== "B" && tipoLibro !== "C") {
-                    console.error("no seleccionaste una opcion válida")
-                    return
-                }
-
-                if (tipoLibro === "A") {
-                    importe = valorFijo *50
-                } else if (tipoLibro === "B") {
-                    importe = valorFijo *100
-                } else {
-                    importe = valorFijo *200  
-                }                   
-                break
-
-            default:
-                console.error("Ocurrió un error inesperado")
-                return
-        }
-        alert("El valor de tu libro es $ " + importe.toFixed(2))
+const armarTablaHTML = (libro)=> {
+    return `<tr>
+                <td><img src="${libro.imagen}" height=250px ></td>
+                <td><h4>${libro.tipo}</h4></td>
+                <td>$ ${libro.precio}</td>
+                <td>
+                    <button id="${libro.codigo}" class="button button-outline" title="Agregar al carrito">🛒</button>
+                </td>
+            </tr>`
 }
 
+//Cargo productos en tabla HTML
 
-
-//OBJETOS Constructores
-
-const IVA = 1.21
-
-class Productox {
-    constructor(nombre, precio, stock) {
-        this.nombre = nombre.toUpperCase()
-        this.precio = precio
-        this.stock = parseInt(stock)
-    }
-        precioFinal() {
-            return (this.precio * IVA).toLocaleString()
-        }
-
-        descontarStock(unidades) {
-            this.stock = this.stock - unidades
-        }  
-}
-
-const producto1 = new Productox("la comunidad del anillo", 2350, 30)
-const producto2 = new Productox("las dos torres", 3150, 50)
-const producto3 = new Productox("las dos torres", 4780, 25)
-
-// ARRAY
-const libros = ["La comunidad del anillo", "Las 2 Torres", "Regreso del Rey", "Los Padecientes" ]
-
-//Accede al DOM 
-function agregarLibro() {
-    let nuevoLibro = prompt("Ingresa un nuevo Título:")
-
-        if (libros.includes(nuevoLibro)) {
-            console.warn(nuevoLibro, "ya está en el listado.")            
+const cargarProductos = (array)=> {
+    let tablaHTML = ""
+        if (array.length > 0) {
+            array.forEach((libro) => tablaHTML += armarTablaHTML(libro))
         } else {
-            libros.push(nuevoLibro)
-            listarLibrosHTML()
+            tablaHTML = "<h2 class='error-libros'>Error al cargar productos.</h2>"
+        }
+        tbody.innerHTML = tablaHTML
+}
+
+//Activa evento CLICK 
+const activarClickBotonesAdd = ()=> {
+    const botonesAdd = document.querySelectorAll("button.button.button-outline")
+          botonesAdd.forEach(btn => {
+            btn.addEventListener("click", (e)=> {
+                let resultado = buscarLibros(e.target.id)
+                    carrito.push(resultado)
+                    guardarCarrito()
+            })
+          })
+}
+
+cargarProductos(libros)
+activarClickBotonesAdd()
+
+const buscarLibros = (codigo)=> libros.find(libro => libro.codigo === parseInt(codigo))
+
+function comprar() {
+    let codigo = prompt(mensajeInicial)
+        if (!parseInt(codigo)) {
+            alert("⛔️ Error en el código ingresado.")
+            return 
+        }
+        let libroElegido = buscarLibros(codigo)
+            carrito.push(libroElegido)
+        let respuesta = confirm("¿Deseas llevar algún otro libro?")
+        if (respuesta) {
+            comprar()
+        } else {
+            finalizarCompra()
         }
 }
 
-function buscarLibro() {
-    let libro = prompt("Ingresa el Libro a buscar:")
-        let posicion = libros.indexOf(libro)
-            if (posicion === -1) {
-                console.warn("No se encontró el libro", libro)
-            } else {
-                alert(libro + " se encuentra en la posición " + posicion)
-            }
-}
-
-function quitarLibro() {
-    let libro = prompt("Ingresa el libro a eliminar:")
-    let posicion =  libros.indexOf(libro)
-        if (posicion > -1) {
-            let eliminado = libros.splice(posicion, 1)
-            console.table(eliminado)
-            listarLibrosHTML()
-        }
-}
-
-function ordenarLibros() {
-    let sort = prompt("Ingrese la forma de ordenamiento. (A ó Z)")
-    if (sort.toUpperCase() === "A") {
-        libros.sort()
-    } else if (sort.toUpperCase() === "Z") {
-        libros.sort().reverse()
+function verCarrito() {
+    if (carrito.length > 0) {
+        const shopping = new Compra(carrito)
+        alert(`El costo total es de $ ${shopping.obtenerSubtotal()}`)
     } else {
-        console.warn("Ingresa un tipo de ordenamiento válido. (A ó Z")
-        return 
+        alert("El carrito está vacío!")
     }
-    listarLibrosHTML()
 }
 
-function listarLibrosHTML() {
-    const listado = document.querySelector("Ol")
-        listado.innerHTML = ""
-        for (libro of libros)
-            listado.innerHTML += `<li>${libro}</li>`
-}
-listarLibrosHTML()
-
-function retornoCard(producto) {
-    return `<div class="card" id="card${producto.id}">
-                <div class="card-image">${producto.imagen}</div>
-                <div class="card-name">${producto.nombre}</div>
-                <div class="card-price">${producto.precio}</div>
-                <div class="card-button">
-                    <button class="button button-outline button-add" id="${producto.id}"" title="Clic para agregar '${producto.nombre}' al carrito">+</button>
-                </div>
-            </div>`
-    }
-    
-function retornoError() {
-return `<div class="card-error">
-        <h2>Houston, tenemos un problema 🔌</h2>
-        <h3>No pudimos cargar los productos. 🤦🏻‍♂️</h3>
-        <h3>Intenta nuevamente en unos instantes...</h3>
-    </div>`
-}
-
+const btnVerCarrito = document.querySelector("button#verCarrito")
+btnVerCarrito.addEventListener("click", verCarrito)
--- js/productos.js
+++ js/productos.js
@@ -1,6 +1,10 @@
-//Libros
-
-const productos = [ { id: 1, imagen: "", nombre: "La Comunidad del Anillo", precio: 3220 },
-                    { id: 2, imagen: "", nombre: "Las dos Torres", precio: 4500 },
-                    { id: 3, imagen: "", nombre: "El Retorno del Rey", precio: 5800 },
-                    { id: 4, imagen: "", nombre: "Los Padecientes", precio: 3350 } ]
+const libros = [{imagen: 'https://images.cdn3.buscalibre.com/fit-in/360x360/54/49/5449ba87a3e457a22dd6d0972b5c261e.jpg', codigo: 1, tipo: 'El señor de los anillos. La comunidad del anillo.', precio: 5600},
+                 {imagen: 'https://img1.od-cdn.com/ImageType-400/5822-1/%7B05572FDC-B056-457A-9DA4-362F2C58497D%7DIMG400.JPG', codigo: 2, tipo: 'El señor de los anillos. Las dos Torres', precio: 7500},
+                 {imagen: 'https://www.planetadelibros.com.ar/usuaris/libros/fotos/48/m_libros/47931_portada_el-senor-de-los-anillos-n-0303-el-retorno-del-rey-ne_j-r-r-tolkien_202203111320.jpg', codigo: 3, tipo: 'El señor de los anillos. El retorno del rey', precio: 8600},
+                 {imagen: 'https://static.wikia.nocookie.net/esharrypotter/images/9/9a/Harry_Potter_y_la_Piedra_Filosofal_Portada_Espa%C3%B1ol.PNG/revision/latest?cb=20151020165725', codigo: 4, tipo: 'Harry Potter y la piedra filosofal', precio: 3450},
+                 {imagen: 'https://static.wikia.nocookie.net/esharrypotter/images/b/b6/Harry_Potter_y_la_Camara_Secreta_Portada_Espa%C3%B1ol.PNG/revision/latest?cb=20151020165931', codigo: 5, tipo: 'Harry Potter y la cámara secreta', precio: 6010},
+                 {imagen: 'https://qph.cf2.quoracdn.net/main-qimg-7bd067ca8385d8657e9d602d64fcbfad-pjlq', codigo: 6, tipo: 'Harry Potter y el prisionero de azkaban', precio: 6653},
+                 {imagen: 'https://http2.mlstatic.com/D_NQ_NP_937011-MLA47578337433_092021-O.jpg', codigo: 7, tipo: 'Harry Potter y el cáliz de fuego', precio: 7952},
+                 {imagen: 'https://static.wikia.nocookie.net/esharrypotter/images/9/9d/Harry_Potter_y_la_Orden_del_F%C3%A9nix_Portada_Espa%C3%B1ol.PNG/revision/latest?cb=20151020170600', codigo: 8, tipo: 'Harry Potter y la Orden del Fénix', precio: 8744},
+                 {imagen: 'https://static.wikia.nocookie.net/esharrypotter/images/8/8d/Harry_Potter_y_el_Misterio_del_Pr%C3%ADncipe_Portada_Espa%C3%B1ol.PNG/revision/latest?cb=20151020170658', codigo: 9, tipo: 'Harry Potter y el misterio del príncipe', precio: 9750},
+                 {imagen: 'https://juanjelopezponeletras.files.wordpress.com/2012/05/harry-potter-y-las-reliquias-de-la-muerte.jpg', codigo: 10, tipo: 'Harry Potter y las reliquias de la muerta', precio: 11203}]
