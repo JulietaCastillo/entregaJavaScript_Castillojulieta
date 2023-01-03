a2a3da534e395d9c73d1298bfc2e47a0e6e6a9ba
Julieta <arq.julietacastillo@gmail.com>
2023-01-03 00:18:47 -0300

    agrego nuevas funciones

--- js/checkout.js
+++ js/checkout.js
@@ -6,20 +6,53 @@ function recuperarCarritoTLL() {
     const carrito = JSON.parse(localStorage.getItem("CarritoTLL"))
     
     if (carrito.length > 0) {
-        carrito.forEach(libro => {
-            tablaHTML += armarTablaCarrito(libro)
-        });
+        carrito.forEach(libro => tablaHTML += armarTablaCarrito(libro))
         tbody.innerHTML = tablaHTML
+        calcularTotal()
     }
 }
 recuperarCarritoTLL()
 
-function activarClickBotones() {
+function activarBotonesDelete() {
     const buttonsDelete = document.querySelectorAll("button.button-add")
     buttonsDelete.forEach(btn => {
         btn.addEventListener("click", ()=> {
+            let pos = carrito.findIndex(libro => libro.nombre === btn.id)
+            if (pos > -1) {
+                carrito.splice(pos, 1)
+                localStorage.setItem("CarritoTLL", JSON.stringify(carrito))
+                recuperarCarritoTLL()
+                activarBotonesDelete()
+            }
         })
     })
 }
+activarBotonesDelete()
 
+function calcularTotal() {
+    let total = document.querySelector("h3#total")
+    let totalCarritoTLL = carrito.reduce((acc, libro)=> acc + libro.precio, 0) 
+        total.innerHTML = `Total: $ ${totalCarritoTLL.toLocaleString()}`
+}
+
+const btnComprar = document.querySelector("#btnComprar")
+
+btnComprar.addEventListener("click", ()=> {
+    Swal.fire({
+        title: '¿Estas seguro que quieres hacer esta compra?',
+        showCancelButton: true,
+        confirmButtonText: 'Confirmar',
+        showCancelButton: 'Cancelar',
+      })
+      .then(result => {
+        if (result.isConfirmed) {
+            localStorage.removeItem("CarritoTLL")
+            carrito.length = 0
+            Swal.fire("Gracias por elegirnos", '')
+                .then(()=> {
+                    location.href = 'index.html'
+                })
+        }
+      }) 
+})
 
